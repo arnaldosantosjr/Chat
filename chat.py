12f9297bc7fe8d25ada2473e8e61128c4d727b9b
Arnaldo Santos <83240835+arnaldosantosjr@users.noreply.github.com>
2025-04-11 22:18:12 -0300

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,16 @@
+# Criar o chat
+
+# Títulho: WebChat
+# Cotão: Iniciar Chat no botão
+    # Quando clicar no botão:
+    # - Criar uma nova janela/Dialog/Modal
+        # Título: Bem-vindo ao WebChat
+        # Campo de texto: Escreva seu nome no chat
+        # Botão: Enviar
+            #Clcou no botão:
+            # Fechar a janela/Dialog/Modal
+                # Criar  o chat
+                # Criar o campo de texto para o chat: Digite sua mensagem
+                # Botão: Enviar
+                    # Quando clicar no botão:
+                    # Enviar a mensagem para o chat
